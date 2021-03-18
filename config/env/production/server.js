0fccf3fb19f47ee994d48ffe0449ddc09e936dc2
dexternierva <dexternierva@gmail.com>
2021-03-18 21:29:10 +0800

    Added PUBLIC_URL in server,js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
 	url: env('STRAPI_URL'),
-	admin: { url: "/admin" }
+	admin: { url: "/admin" },
+	url: env('PUBLIC_URL', 'https://avantage-cms-hobb4.ondigitalocean.app/api'),
 });
