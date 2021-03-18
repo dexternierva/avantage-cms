3a395b2a519a49c733985022044d46c9cf4e9519
dexternierva <dexternierva@gmail.com>
2021-03-18 21:40:42 +0800

    Update server.js

--- config/env/production/server.js
+++ config/env/production/server.js
@@ -1,5 +1,4 @@
 module.exports = ({ env }) => ({
 	url: env('STRAPI_URL'),
-	admin: { url: "/admin" },
-	url: env('PUBLIC_URL', 'https://avantage-cms-hobb4.ondigitalocean.app/api'),
+	admin: { url: "/admin" }
 });
