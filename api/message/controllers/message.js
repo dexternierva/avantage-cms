b9f2a5698d96c057cf8f7659055e79061fc46269
dexternierva <dexternierva@gmail.com>
2021-03-19 09:33:39 +0800

    Added sendgrid email plugin for Messages and Employers Collection Type

--- api/message/controllers/message.js
+++ api/message/controllers/message.js
@@ -1,8 +1,46 @@
 'use strict';
 
 /**
- * Read the documentation (https://strapi.io/documentation/developer-docs/latest/development/backend-customization.html#core-controllers)
- * to customize this controller
- */
+* Read the documentation (https://strapi.io/documentation/developer-docs/latest/concepts/controllers.html#core-controllers)
+* to customize this controller
+*/
 
-module.exports = {};
+const { parseMultipartData, sanitizeEntity } = require('strapi-utils');
+
+module.exports = {
+    /**
+    * Create a record.
+    *
+    * @return {Object}
+    */
+
+    async create(ctx) {
+        let entity;
+
+        if (ctx.is('multipart')) {
+            const { data, files } = parseMultipartData(ctx);
+            entity = await strapi.services.messages.create(data, { files });
+        } else {
+            entity = await strapi.services.messages.create(ctx.request.body);
+        }
+
+        entity = sanitizeEntity(entity, { model: strapi.models.messages });
+
+        // SEND AN EMAIL BY USING THE EMAIL PLUGIN
+        await strapi.plugins['email'].services.email.send({
+            to: 'info@a-vantageinternational.com',
+            from: 'leads@a-vantageinternational.com',
+            cc: 'sampaga@a-vantageinternational.com',
+            bcc: 'recruitment3@a-vantageinternational.com',
+            subject: 'Message From A-vantage Contact Form',
+            text: `
+                Full Name: ${entity.name}
+                Contact Number: ${entity.contact}
+                Email: ${entity.email}
+                Message/Question: ${entity.message}
+            `,
+        });
+
+        return entity;
+    },
+};
